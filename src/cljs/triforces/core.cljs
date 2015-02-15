f3ada1f5ecf9395136d8f6682ebe683099a9d4bb
Nick Tzaperas <nick@nijotz.com>
2015-02-14 21:05:56 -0800

    Basic animation loop

--- src/cljs/triforces/core.cljs
+++ src/cljs/triforces/core.cljs
@@ -1,9 +1,38 @@
 (ns triforces.core)
 
-(defn drawSquare []
-    (let [target (.getElementById js/document "canvas")
-          context (.getContext target "2d")]
-        (.fillRect context 0 0 100 100)))
+(enable-console-print!)
+
+(defn render-scene [[ctx width height] state]
+    (println "Render scene")
+    (set! (. ctx -fillStyle) "#000")
+    (.fillRect ctx 0 0 100 100))
+
+(defn update-state [state]
+    (println "Update state"))
+
+(defn create-state [] {
+    :actors ()})
+
+(defn clear-screen [[ctx width height]]
+    (println "Clear screen")
+    (set! (. ctx -fillStyle) "#FFF")
+    (.clearRect ctx 0 0 width height))
+
+(defn tick [ctx state]
+    (println "Tick")
+    (clear-screen ctx)
+    (render-scene ctx state)
+    (js/setTimeout (fn []
+        (tick ctx (update-state state))) 33))
+
+(defn context [width height]
+    (let [target (.getElementById js/document "canvas")]
+        (println "Canvas: " target)
+        [(.getContext target "2d")
+            (set! (. target -width) width)
+            (set! (. target -height) height)]))
 
 (defn ^:export init []
-    (drawSquare))
+    (let [ctx (context 640 480)] [
+        (println "Context: " ctx)
+        (tick ctx (create-state))]))
