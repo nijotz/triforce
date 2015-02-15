9ac6f1a2324d443b0e375cd918d5a1a16ba32cf6
Nick Tzaperas <nick@nijotz.com>
2015-02-14 23:03:00 -0800

    Needs more debug logging

--- src/cljs/triforces/core.cljs
+++ src/cljs/triforces/core.cljs
@@ -2,18 +2,34 @@
 
 (enable-console-print!)
 
-(def mouse-state (atom {}))
+(def mouse-state (atom {:mousex -1 :mousey -1}))
 
 (defn render-scene [[ctx width height] state]
     (println "Render scene")
     (set! (. ctx -fillStyle) "#000")
-    (.fillRect ctx (@mouse-state :x) (@mouse-state :y) 100 100))
+    (println "Actors: " (get state :actors))
+    (doseq [actor (get state :actors)]
+        (println "Actor: " actor)
+        (.fillRect ctx (get actor :x) (get actor :y) 10 10)))
+
+(defn create-state [] {:actors []})
 
 (defn update-state [state]
-    (println "Update state"))
+    (println "Update state")
+    (let [x (@mouse-state :mousex)
+          y (@mouse-state :mousey)]
+        (if (and (> x 0) (> y 0)) (do
+            (println "x: " x)
+            (println "y: " y)
+            (clear-mouse-state)
+            (create-actor state x y))
+        (identity state))))
 
-(defn create-state [] {
-    :actors ()})
+(defn create-actor [state x y]
+    (println "Create actor")
+    (assoc state
+        :actors
+        (conj (get state :actors) {:x x :y y :color "#F00"})))
 
 (defn clear-screen [[ctx width height]]
     (println "Clear screen")
@@ -22,6 +38,7 @@
 
 (defn tick [ctx state]
     (println "Tick")
+    (println "State: " state)
     (clear-screen ctx)
     (render-scene ctx state)
     (js/setTimeout (fn []
@@ -34,16 +51,20 @@
             (set! (. target -width) width)
             (set! (. target -height) height)]))
 
-
 (defn hook-input-events []
-    (.addEventListener js/document "mousemove"
+    (.addEventListener js/document "click"
         (fn [e]
-            (set-mouse-state :x (. e -clientX))
-            (set-mouse-state :y (. e -clientY))
+            (println "Click")
+            (update-mouse-state (. e -clientX) (. e -clientY))
             false)))
 
-(defn set-mouse-state [code, value]
-    (swap! mouse-state assoc code value))
+(defn update-mouse-state [x y]
+    (swap! mouse-state assoc :mousex x)
+    (swap! mouse-state assoc :mousey y))
+
+(defn clear-mouse-state []
+    (swap! mouse-state assoc :mousex -1)
+    (swap! mouse-state assoc :mousey -1))
 
 (defn ^:export init []
     (let [ctx (context 640 480)] [
