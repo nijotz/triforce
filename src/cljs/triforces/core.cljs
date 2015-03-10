c2dc625d9c98fb2a9183ea426a2ecf76665601e4
Nick Tzaperas <nick@nijotz.com>
2015-03-10 16:26:53 -0700

    Reorg

--- src/cljs/triforces/core.cljs
+++ src/cljs/triforces/core.cljs
@@ -15,21 +15,12 @@
     (/ (reduce + col) (count col)))
 (defn get-timestamp [] (.getTime (js/Date.)))
 
-(defn create-state [context width height] {
-    :context context
-    :ticks 33
-    :ms_per_tick (/ 1000 33)
-    :next_tick (.getTime (js/Date.))
-    :frametimes []
-    :width width
-    :height height
-    :scale 1
-    :actors []})
-
-(defn apply-actors [state func]
-    (assoc state :actors
-        (for [actor (state :actors)]
-            (func state actor))))
+;;;
+; Rendering
+;;;
+(defn clear-screen [ctx width height]
+    (set! (. ctx -fillStyle) "#FFF")
+    (.clearRect ctx 0 0 width height))
 
 (defn render-actor [state actor]
     (let [ctx (state :context)]
@@ -47,11 +38,70 @@
             (.lineTo ctx (nth move-coords 0) (nth move-coords 1)))
         (.stroke ctx) ))
 
+(defn render-scene [state interp]
+    (apply clear-screen (map state '(:context :width :height)))
+    (doseq [actor (state :actors)] (render-actor state actor))
+    (display-fps state))
+
+;;;
+; Updating
+;;;
+(defn update-if-needed [state timestamp]
+    (if (> timestamp (+ (state :next_tick) (* (state :ms_per_tick) 10))) (do
+        (println "Resuming from pause")
+        (update-scene (assoc state :next_tick timestamp)) )
+    ; else if
+    (if (> timestamp (state :next_tick))
+        (update-scene (update-in state [:next_tick]
+            (partial + (state :ms_per_tick)) ))
+    ; else
+        (identity state) )))
+
 (defn update-actors [state]
     (-> state
         move-actors
         avg-actors-heading))
 
+(defn update-scene [state]
+    (-> state
+        handle-mouse-click
+        update-actors))
+
+;;;
+; Actor things
+;;;
+(defn apply-actors [state func]
+    (assoc state :actors
+        (for [actor (state :actors)]
+            (func state actor))))
+
+(defn avg-actors-heading [state]
+    (let [avg-heading (apply avg (map :heading (state :actors)))]
+        (apply-actors state
+            (fn [state actor] (avg-actor-heading avg-heading actor)) )))
+
+(defn avg-actor-heading [avg-heading actor]
+    (update-in actor [:heading] (fn [heading] (avg heading avg-heading))))
+
+(defn move-actor [state actor]
+    (update-in actor [:coords]
+        (fn [coords] (move-vector coords (actor :heading) (actor :velocity))) ))
+
+(defn move-actors [state actor]
+    (apply-actors state move-actor))
+
+(defn create-actor [state coords]
+    (assoc state
+        :actors
+        (conj (get state :actors) {
+            :coords coords
+            :color "#F00"
+            :heading (* (rand1) tau)
+            :velocity 1})))
+
+;;;
+; FPS tracking and display
+;;;
 (defn update-fps [state]
     (update-in state [:frametimes]
         (fn [ft] (subvec (vec (cons (get-timestamp) ft)) 0 (+ 1 (min (count ft) 9)))) ))
@@ -71,16 +121,38 @@
         (.fillText ctx fps-str 5 (state :height))
         (.strokeText ctx fps-str 5 (state :height)) ))
 
-(defn render-scene [state]
-    (apply clear-screen (map state '(:context :width :height)))
-    (doseq [actor (state :actors)] (render-actor state actor))
-    (display-fps state))
+;;;
+; Just mouse things
+;;;
+(def mouse-state (atom {:mousex -1 :mousey -1}))
 
-(defn update-scene [state]
-    (-> state
-        handle-mouse-click
-        update-actors))
+(defn update-mouse-state [x y]
+    (swap! mouse-state assoc :mousex x)
+    (swap! mouse-state assoc :mousey y))
 
+(defn clear-mouse-state []
+    (swap! mouse-state assoc :mousex -1)
+    (swap! mouse-state assoc :mousey -1))
+
+(defn handle-mouse-click [state]
+    (let [x (@mouse-state :mousex)
+          y (@mouse-state :mousey)]
+        (if (and (> x 0) (> y 0)) (do
+            (clear-mouse-state)
+            (create-actor state [x y]) )
+        ; else
+            (identity state))))
+
+(defn hook-input-events []
+    (.addEventListener js/document "click"
+        (fn [e]
+            (println "Click")
+            (update-mouse-state (. e -clientX) (. e -clientY))
+            false)))
+
+;;;
+; Things to eventually be used for triforces (Recycling Bin)
+;;;
 (defn vec-len [& coords]
     (sqrt (apply + (map sqr coords))))
 
@@ -133,44 +205,25 @@
             (+ y (* (sin heading) length)) ))
         coords))
 
-(defn avg-actors-heading [state]
-    (let [avg-heading (apply avg (map :heading (state :actors)))]
-        (apply-actors state
-            (fn [state actor] (avg-actor-heading avg-heading actor)) )))
+;;;
+; Initial state and game loop start
+;;;
+(defn create-state [context width height] {
+    :context context
+    :ticks 33
+    :ms_per_tick (/ 1000 33)
+    :next_tick (.getTime (js/Date.))
+    :frametimes []
+    :width width
+    :height height
+    :scale 1
+    :actors []})
 
-(defn avg-actor-heading [avg-heading actor]
-    (update-in actor [:heading] (fn [heading] (avg heading avg-heading))))
-
-(defn move-actors [state actor]
-    (apply-actors state move-actor))
-
-(defn move-actor [state actor]
-    (update-in actor [:coords]
-        (fn [coords] (move-vector coords (actor :heading) (actor :velocity))) ))
-
-(defn create-actor [state coords]
-    (assoc state
-        :actors
-        (conj (get state :actors) {
-            :coords coords
-            :color "#F00"
-            :heading (* (rand1) tau)
-            :velocity 1})))
-
-(defn clear-screen [ctx width height]
-    (set! (. ctx -fillStyle) "#FFF")
-    (.clearRect ctx 0 0 width height))
-
-(defn update-if-needed [state timestamp]
-    (if (> timestamp (+ (state :next_tick) (* (state :ms_per_tick) 10))) (do
-        (println "Resuming from pause")
-        (update-scene (assoc state :next_tick timestamp)) )
-    ; else if
-    (if (> timestamp (state :next_tick))
-        (update-scene (update-in state [:next_tick]
-            (partial + (state :ms_per_tick)) ))
-    ; else
-        (identity state) )))
+(defn context [width height]
+    (let [target (.getElementById js/document "canvas")]
+        [(.getContext target "2d")
+            (set! (. target -width) width)
+            (set! (. target -height) height)]))
 
 (defn game-loop [state]
     (let [
@@ -180,38 +233,6 @@
         (render-scene state interp)
         (js/requestAnimationFrame (fn []
             (game-loop (update-if-needed (update-fps state) timestamp)) ))))
-
-(defn context [width height]
-    (let [target (.getElementById js/document "canvas")]
-        [(.getContext target "2d")
-            (set! (. target -width) width)
-            (set! (. target -height) height)]))
-
-(defn handle-mouse-click [state]
-    (let [x (@mouse-state :mousex)
-          y (@mouse-state :mousey)]
-        (if (and (> x 0) (> y 0)) (do
-            (clear-mouse-state)
-            (create-actor state [x y]) )
-        ; else
-            (identity state))))
-
-(defn hook-input-events []
-    (.addEventListener js/document "click"
-        (fn [e]
-            (println "Click")
-            (update-mouse-state (. e -clientX) (. e -clientY))
-            false)))
-
-(def mouse-state (atom {:mousex -1 :mousey -1}))
-
-(defn update-mouse-state [x y]
-    (swap! mouse-state assoc :mousex x)
-    (swap! mouse-state assoc :mousey y))
-
-(defn clear-mouse-state []
-    (swap! mouse-state assoc :mousex -1)
-    (swap! mouse-state assoc :mousey -1))
 
 (defn ^:export init []
     (hook-input-events)
